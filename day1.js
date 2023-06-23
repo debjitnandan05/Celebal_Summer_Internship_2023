7f521bf802fe61b1bf9b79fc5dc4b6e1b9cb080e
DEBJIT NANDAN <debjitnandan05@gmail.com>
2023-06-23 22:53:19 +0530

    operator section  added

--- day1.js
+++ day1.js
@@ -48,6 +48,57 @@ const Student = 'Nandan';
 console.log(Student);
 console.log(student);
 
+//--------------- operator ----------------
+// 1. Arrithmatic operator
+var a = 5;
+var b = 10;
+
+// console.log(a+b); // addition
+// console.log(b-a); // substraction
+// console.log(b*a); // multiplication
+// console.log(b/a); // division
+// console.log(11%a); // modulo
+// console.log(10**2); // Exponentiation
+// a++; // increment
+// var inc =a ;
+// b--; // decrement   
+// var dec =b;
+// console.log(inc);
+// console.log(dec);
+
+// 2. Assignment operator
+
+// let m= 30;
+// let n =20;
+// m+=10; // Addition Assignment Operator
+// console.log(m)
+// n-=10; // Substraction Assignment Operator
+// console.log(n);
+// m*=10; // Multiplication Assignment Operator
+// console.log(m)
+// n/=10; // Division Assignment Operator
+// console.log(n);
+// m%=10; // Modulo Assignment Operator
+// console.log(m);
+
+// let f = 100;
+// f <<= 5; // Left Shift Assignment Operator 
+// console.log(f);
+// f >>= 8; // Right Shift Assignment Operator 
+// console.log(f);
+
+
+// 3. Logical operator 
+
+let t = 100;
+t &= 5; //Bitwise AND Assignment Operator
+console.log(t);
+t|=5; //Bitwise OR Assignment Operator
+console.log(t);
+t^=5; //Bitwise XOR Assignment Operator
+console.log(t);
+
+
 
 //----------------- Datatypes --------------------
 
@@ -59,13 +110,23 @@ const color = "Yellow"; // String
 const person = {fName:"Debjit", lName:"Nandan"}; // Object
 const cars = ["Royal Enfield", "KTM", "Honda", "Hero"]; //Array
 const date = new Date(); //Date
-let x; // undefined
+// let x; // undefined
 
-console.log(length);
-console.log(color);
-console.log(person);
-console.log(cars);
-console.log(date);
+
+// console.log(color);
+// console.log(person);
+// console.log(length);
+// console.log(cars);
+// console.log(date);
+
+const pupil = {         // object
+    fName: "Debjit",
+    lName: "nandan",
+    age: 25,
+    roll : 20101105042
+  };
+
+  console.log(pupil.fName);
 
 
 
